da0470c2ad565a27cb68f07adfa5e6d2da11e402
yezixx <skylow917@daum.net>
2022-11-03 13:56:37 +0900

    add router

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,13 @@ import React from "react";
 import styled from "styled-components";
 import CheeseImage from "../assets/cheese.jpeg";
 import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const handleClickButton = () => {
+    navigate("/question");
+  };
   return (
     <Wrapper>
       <Header>예비집사 판별기</Header>
@@ -19,7 +24,9 @@ const Home = () => {
           />
         </LogoImage>
         <Desc>MBTI를 기반으로 하는 나랑 잘맞는 고양이 찾기!</Desc>
-        <Button>테스트 시작하기</Button>
+        <Button style={{ fontFamily: "HBIOS-SYS" }} onClick={handleClickButton}>
+          테스트 시작하기
+        </Button>
       </Content>
     </Wrapper>
   );
@@ -37,6 +44,7 @@ const Header = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  font-family: "HBIOS-SYS";
 `;
 
 const Content = styled.div`
@@ -49,6 +57,7 @@ const Content = styled.div`
 const Title = styled.div`
   font-size: 30pt;
   margin-top: 40px;
+  font-family: "HBIOS-SYS";
 `;
 
 const LogoImage = styled.div`
@@ -58,4 +67,5 @@ const LogoImage = styled.div`
 const Desc = styled.div`
   font-size: 20pt;
   margin-top: 20px;
+  font-family: "HBIOS-SYS";
 `;
