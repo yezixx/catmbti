8be52ce744e932f2bbe55228541b0fe8be3daf74
yezixx <skylow917@daum.net>
2022-11-05 16:43:29 +0900

    add question

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -1,7 +1,54 @@
 import React from "react";
+import styled from "styled-components";
+import { ProgressBar, Button } from "react-bootstrap";
+import { QuestionData } from "../assets/data/questiondata";
 
 const Question = () => {
-  return <div>홈화면입니다.</div>;
+  return (
+    <Wrapper>
+      <ProgressBar
+        striped
+        variant="danger"
+        now={80}
+        style={{ marginTop: "20px" }}
+      />
+      <Title>{QuestionData[0].title}</Title>
+      <ButtonGroup>
+        <Button style={{ width: "40%", minHeight: "200px", fontsize: "15pt" }}>
+          {QuestionData[0].answera}
+        </Button>
+        <Button
+          style={{
+            width: "40%",
+            minHeight: "200px",
+            fontsize: "15pt",
+            marginLeft: "20px",
+          }}
+        >
+          {QuestionData[0].answerb}
+        </Button>
+      </ButtonGroup>
+    </Wrapper>
+  );
 };
 
 export default Question;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100%;
+`;
+
+const Title = styled.div`
+  font-size: 30pt;
+  text-align: center;
+  font-family: "HBIOS-SYS";
+`;
+
+const ButtonGroup = styled.div`
+display:flex;
+flex-direction:row;
+align:items:center;
+justify-content:center;
+font-family: "HBIOS-SYS";
+`;
