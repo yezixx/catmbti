8ba7257978a2fa4ef9e9fe362a14c04c600eb1da
yezixx <skylow917@daum.net>
2022-11-13 19:43:01 +0900

    에러 수정 3

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
