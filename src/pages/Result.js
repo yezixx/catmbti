c9e45da3c8a52eecd01d415f5bf321b24d31556f
yezixx <skylow917@daum.net>
2022-11-13 18:44:33 +0900

    add result data

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,7 +1,74 @@
 import React from "react";
+// css-in-js 방법으로
+import styled from "styled-components";
+// import CheeseImage from "../assets/cheese.jpeg";
+import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
+import { ResultData } from "../assets/data/resultdata";
 
 const Result = () => {
-  return <div>결과화면입니다.</div>;
+  const navigate = useNavigate();
+  return (
+    <Wrapper>
+      <Header>예비집사 판별기</Header>
+      <Content>
+        <Title>결과 보기</Title>
+        <LogoImage>
+          <img
+            src={ResultData[0].image}
+            className="rounded-circle"
+            width={350}
+            height={350}
+          />
+        </LogoImage>
+        <Desc>
+          예비 집사님과 찰떡궁합인 고양이는 {ResultData[0].name}입니다.
+        </Desc>
+        <Button
+          style={{ fontFamily: "HBIOS-SYS" }}
+          onClick={() => navigate("/")}
+        >
+          테스트 다시하기
+        </Button>
+      </Content>
+    </Wrapper>
+  );
 };
 
 export default Result;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100%;
+`;
+
+const Header = styled.div`
+  font-size: 40pt;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "HBIOS-SYS";
+`;
+
+const Content = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Title = styled.div`
+  font-size: 30pt;
+  margin-top: 40px;
+  font-family: "HBIOS-SYS";
+`;
+
+const LogoImage = styled.div`
+  margin-top: 10px;
+`;
+
+const Desc = styled.div`
+  font-size: 20pt;
+  margin-top: 20px;
+  font-family: "HBIOS-SYS";
+`;
