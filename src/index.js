f8b2e5a8fd37001003a521f3f0cb7d9cf6ca1bef
yezixx <skylow917@daum.net>
2022-12-07 14:45:41 +0900

    add copy

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+const Copy = () => {
+  return <p>&copy; HBIOS-SYS</p>;
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    <Copy />
   </React.StrictMode>
 );
