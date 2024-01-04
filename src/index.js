a1eb0ebccbb3ebfe923af7f9af034a093773da05
yezixx <skylow917@daum.net>
2024-01-04 21:32:03 +0900

    오타 수정

--- src/index.js
+++ src/index.js
@@ -5,16 +5,11 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-var Copy = () => {
-  <p>&copy; HBIOS-SYS</p>;
-};
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-      <Copy />
     </BrowserRouter>
   </React.StrictMode>
 );
